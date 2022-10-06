3ff8ce4bd124fee27b7e5277f7b08a78a13da120
Adrian Baltag <fbaltag83@gmail.com>
2022-10-06 10:59:03 -0400

    set up GUI

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 from tkinter import *
 from tkinter import filedialog
+
+# Tk application object created by instantiating Tk
+screen = Tk()
+title = screen.title('YouTube Downloader')
+# set up GUI
+canvas = Canvas(screen, width=500, height=500)
+canvas.pack()
+
+
+# run GUI
+screen.mainloop()
