4beeb81554b743404cff6c96e6f7a33c2dc3cafb
Adrian Baltag <fbaltag83@gmail.com>
2022-10-06 11:04:46 -0400

    set up img-logo for canvas

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ title = screen.title('YouTube Downloader')
 canvas = Canvas(screen, width=500, height=500)
 canvas.pack()
 
+# img-logo
+logo_img = PhotoImage(file='yt.png')
+# resize img
+logo_img = logo_img.subsample(4, 4)
+# set up img to be displayed on canvas
+canvas.create_image(250, 80, image=logo_img)
 
 # run GUI
 screen.mainloop()
