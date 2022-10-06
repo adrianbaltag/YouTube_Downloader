c491fa4108911030c6da8a5c824831cc24d02389
Adrian Baltag <fbaltag83@gmail.com>
2022-10-06 11:18:07 -0400

    added the SELECT and DOWNLOAD BTN

--- app.py
+++ app.py
@@ -21,9 +21,22 @@ canvas.create_image(250, 80, image=logo_img)
 link_field = Entry(screen, width=50)
 link_label = Label(screen, text="Enter Download Link:", font="Lato, 15")
 
+# select path for saving file
+path_label = Label(screen, text="Select path for download")
+select_btn = Button(screen, text='Select')
+
+# add to window
+canvas.create_window(250, 280, window=path_label)
+canvas.create_window(250, 330, window=select_btn)
+
 # add link widgets to window
 canvas.create_window(250, 170, window=link_label)
 canvas.create_window(250, 200, window=link_field)
+
+# download btns
+download_btn = Button(screen, text="Download file")  # , command=download_file
+# add to canvas
+canvas.create_window(250, 390, window=download_btn)
 
 
 # run GUI
