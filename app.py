2077a8738148f883e0a1572a6f2f778d30e16f99
Adrian Baltag <fbaltag83@gmail.com>
2022-10-12 14:04:57 -0400

    added functionality to delete allbtn

--- app.py
+++ app.py
@@ -8,17 +8,34 @@ from moviepy.editor import VideoFileClip
 from pytube import YouTube
 # allows you to copy files/folder and move it
 import shutil
+import os
+import time
 
-# functions
 
 
-# delete btn input
+
+            # functions
+# clear btn input
 def delete_link():
     list.delete(0, "end")
     link_field.delete(0, 'end')
 
 
+# delete file from pc folder
+def del_video():
+    target = "C:\\Users\\adria\\OneDrive\\Desktop\\video_download\\"
+    for video in os.listdir(target):
+        if video.endswith('.mp4'):
+            print('deleting file:', video)
+            os.unlink(target + video)
+            time.sleep(2)
+            print('file deleted')
+
+
 # allows user to select a path from the explorer
+print(os.getcwd())
+
+
 def select_path():
     path = filedialog.askdirectory()
     path_label.config(text=path)
@@ -27,6 +44,8 @@ def select_path():
 def download_file():
     global value_link
     global get_link
+    global user_path
+    global mp4_video
     # get user path
     get_link = link_field.get()
     # display user choice in listbox
@@ -85,19 +104,19 @@ download_btn = Button(screen, text="Download file", padx=19, bg='#c92a2a', fg='#
 canvas.create_window(250, 360, window=download_btn)
 
 # frame
-frame = Frame(canvas, width=450, height=200, border=0)
+frame = Frame(canvas, width=450, height=50)
 # add frame to canvas
 canvas.create_window(250, 520, window=frame)
 
-list = Listbox(frame, width=80, height=15, background='#c92a2a', fg='white')
+list = Listbox(frame, width=80, height=5, background='#c92a2a', fg='white')
 list.pack()
 
 # btn interact with listbox
-btn_delete = Button(canvas, text="Delete", bg='white', fg='red', padx=20, command=delete_link)
+btn_delete = Button(canvas, text="Clear", bg='white', fg='red', padx=20, command=delete_link)
 # add btn to canvas
 canvas.create_window(100, 660, window=btn_delete)
 
-btn_delete_all = Button(canvas, text="Delete All", bg='white', fg='red', padx=18)
+btn_delete_all = Button(canvas, text="Delete All", bg='white', fg='red', padx=18, command=del_video)
 # add btn to canvas
 canvas.create_window(200, 660, window=btn_delete_all)
 
